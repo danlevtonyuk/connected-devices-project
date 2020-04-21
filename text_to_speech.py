30ce3e927e2033092c02323540326eff8c7bdbd1
arsh <arsh@arsh-Precision-5520.com>
2020-04-20 22:25:34 -0400

    added voice

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,17 +1,17 @@
 from gtts import gTTS 
 import os 
   
-mytext = 'Welcome to geeksforgeeks!'
+mytext = 'I didnt get that. Please say again!'
   
 language = 'en'
   
 myobj = gTTS(text=mytext, lang=language, slow=False) 
   
-myobj.save("welcome.mp3") 
+myobj.save("say_again.mp3") 
   
 import pygame, time
 pygame.init()
-pygame.mixer.music.load('welcome.mp3')
+pygame.mixer.music.load('say_again.mp3')
 pygame.mixer.music.play()
 time.sleep(5)
 pygame.mixer.music.fadeout(5)
